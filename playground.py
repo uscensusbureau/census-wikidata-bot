937497fe45bb3782e321b1d08a1c14745846a9a1
Logan Powell <loganp@tepper.cmu.edu>
2019-05-23 14:23:52 -0400

    understanding map, reduce, etc...

--- playground.py
+++ playground.py
@@ -1,5 +1,10 @@
-import requests, json, logging, sys
+# from arepldump import dump
 
+import requests, json, logging, sys, queue, pprint, functools
+
+prn = pprint.PrettyPrinter(indent=2)
+pipe = queue.Queue()
+reduce = functools.reduce
 
 def get_census_values(api_url, get_var, for_var):
     try:
@@ -13,7 +18,34 @@ def get_census_values(api_url, get_var, for_var):
         logging.error('IOError: {}'.format(err))
         
         
-result = get_census_values("https://api.census.gov/data/2017/acs/acs5", "NAME", "state:*")
 
-print(result)
+def create_census_json(arr):
+    keys_arr = arr[0]
+    vals_arr = arr[1:]
+    # return vals_arr
 
+    result = list(map(lambda vals: 
+        reduce(lambda acc, this: 
+            acc.update({keys_arr[this[0]]: this[1]}) or acc, 
+            enumerate(vals),
+            {}),
+        vals_arr))
+    return result
+
+        
+pipe.put(get_census_values("https://api.census.gov/data/2017/acs/acs5", "NAME,B01001_001E", "state:*"))
+
+# prn.pprint(list(map(lambda x: x, pipe.get())))
+prn.pprint(create_census_json(pipe.get()))
+
+# test_arr = [["key1", "key2", "key3"], [1, 2, 3], [4, 5, 6]]
+# print(create_census_json(test_arr))
+
+
+
+
+print("test {}".format("me"))
+
+me = "Tarzan"
+
+print(f"Me {me}")
